7a7254754b0d84c27d86522faa629013f26243ec
nikhilitis <nrao@ucsd.edu>
2022-11-07 00:32:27 -0800

    completed expose part of the lab

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,11 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  let recipes = localStorage.getItem("recipes");
+  if (!localStorage) return [];
+
+  let recipesObject = JSON.parse(recipes);
+  return recipesObject;
 }
 
 /**
@@ -35,10 +40,29 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  let mainElement = document.querySelector("main");
+
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for (let i=0; i<recipes.length; i++){
+    let recipeCard = document.createElement('recipe-card');
+
+    recipeCard.data = {
+      "imgSrc": recipes[i].imgSrc,
+      "imgAlt": recipes[i].imgAlt,
+      "titleLnk": recipes[i].titleLnk,
+      "titleTxt": recipes[i].titleTxt,
+      "organization": recipes[i].organization,
+      "rating": recipes[i].rating,
+      "numRatings": recipes[i].numRatings,
+      "lengthTime": recipes[i].lengthTime,
+      "ingredients": recipes[i].ingredients
+    };
+    mainElement.append(recipeCard);
+  }
+
 }
 
 /**
